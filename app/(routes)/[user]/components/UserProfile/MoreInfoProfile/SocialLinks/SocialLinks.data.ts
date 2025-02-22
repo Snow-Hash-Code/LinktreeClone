727a24164cd21439ba781af437a051bd4eea9e02
Snow-Hash-Code <gerardoluis206@gmail.com>
2025-02-22 02:51:33 -0600

    socialLinksdata actualizado

--- app/(routes)/[user]/components/UserProfile/MoreInfoProfile/SocialLinks/SocialLinks.data.ts
+++ app/(routes)/[user]/components/UserProfile/MoreInfoProfile/SocialLinks/SocialLinks.data.ts
@@ -2,25 +2,25 @@ export const dataLinks = [
   {
     id: 1,
     icon: '/social-networks/facebook.png',
-    link: 'https://www.facebook.com/sharer/sharer.php?u=https://tarretreeclone.com',
+    link: 'https://www.facebook.com/sharer/sharer.php?u=https://linktree-clone-topaz-nu.vercel.app',
     text: "Facebook"
   },
   {
     id: 2,
     icon: '/social-networks/linkedin.png',
-    link: 'https://www.linkedin.com/sharing/share-offsite/?url=https://tarretreeclone.com',
+    link: 'https://www.linkedin.com/sharing/share-offsite/?url=https://linktree-clone-topaz-nu.vercel.app',
     text: "LinkedIn"
   },
   {
     id: 3,
     icon: '/social-networks/whatsapp.png',
-    link: 'https://api.whatsapp.com/send?text=https://tarretreeclone.com',
+    link: 'https://api.whatsapp.com/send?text=https://linktree-clone-topaz-nu.vercel.app',
     text: "Whatsapp"
   },
   {
     id: 4,
     icon: '/social-networks/x.png',
-    link: 'https://twitter.com/intent/tweet?url=https://tarretreeclone.com',
+    link: 'https://twitter.com/intent/tweet?url=https://linktree-clone-topaz-nu.vercel.app',
     text: "X"
   }
 ]
